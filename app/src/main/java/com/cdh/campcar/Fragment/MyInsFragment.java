cfb26e41c17189b566f2ecc691e9ec69699bc685
cdhgold <cdhgold@naver.com>
2020-05-10 22:07:13 +0900

    0510

--- app/src/main/java/com/cdh/campcar/Fragment/MyInsFragment.java
+++ app/src/main/java/com/cdh/campcar/Fragment/MyInsFragment.java
@@ -109,10 +109,7 @@ public class MyInsFragment extends Fragment implements View.OnClickListener {
             //   서버로 값 전송
             //--------------------------
             StringBuffer buffer = new StringBuffer();
-            buffer.append("id").append("=").append(myId).append("&");                 // php 변수에 값 대입
-            buffer.append("pword").append("=").append(myPWord).append("&");   // php 변수 앞에 '$' 붙이지 않는다
-            buffer.append("title").append("=").append(myTitle).append("&");           // 변수 구분은 '&' 사용
-            buffer.append("subject").append("=").append(mySubject);
+//            buffer.append("id").append("=").append(myId).append("&");                 // php 변수에 값 대입
 
             OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
             PrintWriter writer = new PrintWriter(outStream);
@@ -128,9 +125,9 @@ public class MyInsFragment extends Fragment implements View.OnClickListener {
             while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
                 builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
             }
-            myResult = builder.toString();                       // 전송결과를 전역 변수에 저장
-            ((TextView)(findViewById(R.id.text_result))).setText(myResult);
-            Toast.makeText(MainActivity.this, "전송 후 결과 받음", 0).show();
+//            myResult = builder.toString();                       // 전송결과를 전역 변수에 저장
+//            ((TextView)(findViewById(R.id.text_result))).setText(myResult);
+//            Toast.makeText(MainActivity.this, "전송 후 결과 받음", 0).show();
         } catch (MalformedURLException e) {
 
         } catch(IOException e){
